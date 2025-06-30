f0abea9a22e802ad6db62d47c9672aa7ceb442df
Felipe Yepez <felipeyepez@google.com>
2025-06-30 08:29:39 +0000

    Prevent L4 ILB controller race conditions on service creation
    Introduces a 3-second wait and re-fetch mechanism when a service has no L4 ILB finalizers (v1 or v2). This prevents race conditions between controllers during initial service creation, ensuring the L4 controller processes the most recent service state and avoids conflicting operations. This to prevent race during cluster update when enabling GKE subsetting.

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -147,6 +147,8 @@ var F = struct {
 	OverrideHealthCheckSourceCIDRs            string
 	ManageL4LBLogging                         bool
 
+	L4ILBLegacyHeadStartTime time.Duration
+
 	// ===============================
 	// DEPRECATED FLAGS
 	// ===============================
@@ -356,6 +358,8 @@ L7 load balancing. CSV values accepted. Example: -node-port-ranges=80,8080,400-5
 	flag.StringVar(&F.OverrideHealthCheckSourceCIDRs, "override-health-check-src-cidrs", "", "Overrides the default source IP ranges used when configuring firewall rules to allow health check probes for L7 load balancers. Provide the ranges as a comma-separated list of CIDRs. Example: --override-health-check-src-cidrs=130.211.0.0/22,35.191.0.0/16")
 	flag.BoolVar(&F.ManageL4LBLogging, "manage-l4lb-logging", false, "Manage L4 ILB/NetLB logging.")
 	flag.BoolVar(&F.ReadOnlyMode, "read-only-controllers", false, "When enabled, this flag runs the IG, NEG, L4 ILB, and L4 NetLB controllers in a read-only mode. This prevents them from executing any mutating API calls (e.g., create, update, delete), allowing you to safely observe controller behavior without modifying resources. The Ingress controller is exempt from this mode.")
+
+	flag.DurationVar(&F.L4ILBLegacyHeadStartTime, "prevent-legacy-race-l4-ilb", 0*time.Second, "When enabled, this flag fetches the last state of the ILB after a delay to prevent controllers race when the service is created.")
 }
 
 func Validate() {
